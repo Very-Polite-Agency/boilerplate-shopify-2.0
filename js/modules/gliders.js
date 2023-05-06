78d15127fa6e43f735055a63069b950df50420cd
Brenden Mersey <brenden.mersey@weareverypolite.com>
2023-05-05 17:04:58 -0700

    QC

--- js/modules/gliders.js
+++ js/modules/gliders.js
@@ -1,116 +1,70 @@
-const config = { debug: false, name: 'gliders.js', version: '1.0' };
-const elements = document.querySelectorAll( '.js--glide' ) || [];
+import Glide from '@glidejs/glide';
+
+const config = { debug: true, name: 'gliders.js', version: '1.0' };
+
 const events = [ "build.after", "run.after" ];
+const elements = document.querySelectorAll( '.js--glide' ) || [];
 const gliders = {};
 
 const createGliderFromElement = ( element = {} ) => {
 
   let element_id = element?.id ?? '';
-  let animationDuration = parseInt( element.dataset?.glideAnimationDuration ) ?? 450;
-  let autoplay = parseInt( element.dataset?.glideAutoplay ) ?? 3500;
-  let gap = parseInt( element.dataset?.glideGap ) ?? 36;
+  let animationDuration = parseInt( element.dataset?.glideAnimationDuration ?? 450 );
+  let autoplay = parseInt( element.dataset?.glideAutoplay ?? 3500 );
+  let direction = element.dataset?.glideDirection ?? 'rtl';
+  let gap = parseInt( element.dataset?.glideGap ?? 36 );
   let style = element.dataset?.glideStyle ?? '';
-  let options = getOptions({ animationDuration, autoplay, gap });
+
+  let options = getOptions({
+    animationDuration,
+    autoplay,
+    direction,
+    gap
+  });
 
   switch ( style ) {
-    case 'awards': {
-      options = getOptions({
-        animationDuration,
-        autoplay,
-        breakpoints: {
-          991: {
-            peek: { before: 0, after: 150 },
-            perView: 2
-          },
-          767: {
-            peek: { before: 0, after: 150 },
-            perView: 1
-          },
-          575: {
-            peek: { before: 0, after: 75 },
-            perView: 1
-          }
-        },
-        gap,
-      });
-      break;
-    }
-    case 'product-carousel': {
-       options = getOptions({
-        animationDuration,
-        autoplay,
-        breakpoints: {
-          9999: {
-            peek: { before: 0, after: 160 },
-            perView: 5
-          },
-          1799: {
-            peek: { before: 0, after: 160 },
-            perView: 4
-          },
-          1399: {
-            peek: { before: 0, after: 160 },
-            perView: 3
-          },
-          1199: {
-            peek: { before: 0, after: 160 },
-            perView: 3
-          },
-          991: {
-            peek: { before: 0, after: 160 },
-            perView: 2
-          },
-          767: {
-            peek: { before: 0, after: 160 },
-            perView: 1
-          },
-          575: {
-            peek: { before: 0, after: 80 },
-            perView: 1
-          }
-        },
-        gap,
-        hoverpause: true,
-      });
+    case 'announcements': {
+      options.hoverpause = true;
       break;
     }
   }
 
-  if ( element_id ) {
+  let glide = new Glide( "#" + element_id, options );
 
-    let glide = new Glide( `#${element_id}`, options );
-
-    glide.on( events, event => {
-
-      switch ( style ) {
-        default: {
-          setTimeout( () => updateGlideTrackHeight( element ), 100 );
-          break;
-        }
+  glide.on( events, event => {
+    switch ( style ) {
+      case 'product-image-gallery-carousel': {
+        updateProductGalleryModalTrigger( glide.index );
+        break;
       }
+      default: {
+        setTimeout( () => updateGlideTrackHeight( element ), 100 );
+        break;
+      }
+    }
+  });
 
+  ( document.querySelectorAll( '[data-glide-navigation="#' + element_id + '"].next, [data-target="#' + element_id + '"].next' ) || [] ).forEach( button => {
+    button.addEventListener("click", function () {
+      glide.go(">");
     });
+  });
 
-    ( document.querySelectorAll( `[data-glide-navigation="#${element_id}"].next, [data-target="#${element_id}"].next`  ) || [] ).forEach( button => {
-      button.addEventListener("click", function () {
-        glide.go(">");
-      });
+  ( document.querySelectorAll( '[data-glide-navigation="#' + element_id + '"].prev, [data-target="#' + element_id + '"].prev' ) || [] ).forEach( button => {
+    button.addEventListener("click", function () {
+      glide.go("<");
     });
+  });
 
-    ( document.querySelectorAll( `[data-glide-navigation="#${element_id}"].prev, [data-target="#${element_id}"].prev` ) || [] ).forEach( button => {
-      button.addEventListener("click", function () {
-        glide.go("<");
-      });
-    });
+  glide.mount();
 
-    glide.mount();
+  // FIX for when single slide does not fill 100% of glider
+  setTimeout( () => {
+    console.log( element_id, style, options );
+     glide.update();
+   }, 250 );
 
-    // FIX for when single slide does not fill 100% of glider
-    setTimeout( () => { glide.update() }, 300 );
-
-    gliders[element_id] = { element_id, glide };
-
-  }
+  gliders[element_id] = { element_id, glide };
 
 };
 
@@ -120,7 +74,8 @@ const getOptions = ( custom = {} ) => {
     animationTimingFunc: "ease-in-out",
     animationDuration: 350,
     autoHeight: true,
-    autoplay: 5000,
+    autoplay: 3250,
+    direction: 'rtl',
     dragThreshold: 35,
     hoverpause: false,
     perView: 1,
@@ -148,15 +103,16 @@ const updateGlideTrackHeight = ( element = false ) => {
   }
 };
 
+const updateProductGalleryModalTrigger = ( index = 0 ) => {
+  ( document.querySelectorAll( '.main-product__image-gallery-modal-trigger' ) || [] ).forEach( button => {
+     button.dataset.productImageGalleryIndex = index;
+  });
+};
+
 const init = () => {
   if ( config.debug ) console.log(`[ ${config.name} v.${config.version} initialized ]`);
     elements.forEach( element => createGliderFromElement( element ) );
   if ( config.debug ) console.log(`[ ${config.name} v.${config.version} complete ]`);
 };
 
-export default {
-  createGliderFromElement,
-  getOptions,
-  gliders,
-  init
-};
+export default { gliders, init };
